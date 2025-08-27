139aa118deec174931da4be42042a1f0aa49039c
artyom165 <artyom.moser@jvm.com>
2025-08-27 11:34:50 +0200

    modified:   src/components/text.tsx

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-
-function Text() {
-  return React.createElement(
-    "div",
-    {
-      style: {
+export default function Text() {
+  return (
+    <div
+      style={{
         padding: "20px",
         background: "blue",
         color: "white",
-      },
-    },
-    React.createElement("p", null, "Hello World")
+      }}
+    >
+      <p>Hello World</p>
+    </div>
   );
 }
-
-export default Text;
