fe35cf9d76b0332383bbdf824c64c17b9a42e465
artyom165 <artyom.moser@jvm.com>
2025-08-27 11:29:26 +0200

    modified:   src/components/text.tsx

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,7 +1,13 @@
 export default function Text() {
   return (
-    <div className="w-20 h-20 bg-red-500">
-      <p>hello</p>
+    <div
+      style={{
+        padding: "20px",
+        background: "blue",
+        color: "white",
+      }}
+    >
+      <p>Hello World</p>
     </div>
   );
 }
